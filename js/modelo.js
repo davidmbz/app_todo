b1df4ad328515b1db94a634e91e57572330c4072
David Martín <dmartinblazquez@gmail.com>
2020-07-21 20:07:41 +0200

    Linkeados todos los archivos con el index y creación del array dentro del modelo

---
+++ js/modelo.js
@@ -0,0 +1,17 @@
+let listaTareas = new Array(
+    {
+        idTarea: 1,
+        titulo: 'Estudiar Javascript',
+        prioridad: 'Urgente'
+    },
+    {
+        idTarea: 2,
+        titulo: 'Dormir',
+        prioridad: 'Diaria'
+    },
+    {
+        idTarea: 3,
+        titulo: 'Salir a comer',
+        prioridad: 'Mensual'
+    }
+);
